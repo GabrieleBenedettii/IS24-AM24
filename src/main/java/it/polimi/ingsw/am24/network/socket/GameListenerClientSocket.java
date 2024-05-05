c221b95de54e5c11d9bb4250e20da7ae38baea28
CanepariMichele <michele.canpari@mail.polimi.it>
2024-05-05 19:11:14 +0200

    added socket method

--- src/main/java/it/polimi/ingsw/am24/network/socket/GameListenerClientSocket.java
+++ src/main/java/it/polimi/ingsw/am24/network/socket/GameListenerClientSocket.java
@@ -1,18 +1,43 @@
 package it.polimi.ingsw.am24.network.socket;
 
-import it.polimi.ingsw.am24.messages.servetToClient.NoLobbyAvaiableMessage;
+import it.polimi.ingsw.am24.listeners.GameListener;
+import it.polimi.ingsw.am24.messages.servetToClient.*;
+import it.polimi.ingsw.am24.modelView.GameCardView;
+import it.polimi.ingsw.am24.modelView.GameView;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class GameListenerClientSocket implements /*GameListener,*/ Serializable {
+public class GameListenerClientSocket implements GameListener, Serializable {
     //todo add all game listener methods
     private final ObjectOutputStream out;
 
     public GameListenerClientSocket(ObjectOutputStream out) {
         this.out = out;
+    }
+
+    @Override
+    public void invalidNumPlayers() throws RemoteException {
+        try {
+            out.writeObject(new InvalidNumPlayersMessage());
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void playerJoined(ArrayList<String> players) throws RemoteException {
+        try {
+            out.writeObject(new PlayerJoinedMessage(players));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
     }
 
     //@Override
@@ -22,8 +47,116 @@ public class GameListenerClientSocket implements /*GameListener,*/ Serializable 
             out.flush();
             out.reset();
 
-        } catch (IOException ignored) {
+        } catch (IOException ignored) {}
+    }
 
-        }
+    @Override
+    public void nicknameAlreadyUsed() throws RemoteException {
+        try {
+            out.writeObject(new NicknameAlreadyUsedMessage());
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void availableColors(ArrayList<String> colors) throws RemoteException {
+        try {
+            out.writeObject(new AvailableColorsMessage(colors));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void hiddenGoalChoice(ArrayList<GameCardView> cardViews) throws RemoteException {
+        try {
+            out.writeObject(new HiddenGoalChoiceMessage(cardViews));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void initialCardSide(GameCardView front, GameCardView back) throws RemoteException {
+        try {
+            out.writeObject(new InitialCardSideMessage(front, back));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void beginTurn(GameView gameView) throws RemoteException {
+        try {
+            out.writeObject(new BeginTurnMessage(gameView));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void invalidPositioning() throws RemoteException {
+        try {
+            out.writeObject(new InvalidPositioningMessage());
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void requirementsNotMet() throws RemoteException {
+        try {
+            out.writeObject(new RequirementsNotMetMessage());
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void beginDraw() throws RemoteException {
+        try {
+            out.writeObject(new BeginDrawMessage());
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void wrongCardPlay(GameView gameView) throws RemoteException {
+        try {
+            out.writeObject(new WrongCardPlayMessage(gameView));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void gameEnded(String winner, HashMap<String, Integer> rank) throws RemoteException {
+        try {
+            out.writeObject(new GameEndedMessage(winner,rank));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
+    }
+
+    @Override
+    public void sentMessage(String message) throws RemoteException {
+        try {
+            out.writeObject(new SentMessageMessage(message));
+            out.flush();
+            out.reset();
+
+        } catch (IOException ignored) {}
     }
 }
--- src/main/java/it/polimi/ingsw/am24/network/socket/SocketClient.java
+++ src/main/java/it/polimi/ingsw/am24/network/socket/SocketClient.java
@@ -1,16 +1,19 @@
 package it.polimi.ingsw.am24.network.socket;
 
 import it.polimi.ingsw.am24.messages.*;
-import it.polimi.ingsw.am24.messages.clientToServer.AddPlayerMessage;
+import it.polimi.ingsw.am24.messages.clientToServer.*;
 import it.polimi.ingsw.am24.network.GameListenerClient;
+import it.polimi.ingsw.am24.view.flow.CommonClientActions;
 import it.polimi.ingsw.am24.view.flow.Flow;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
-public class SocketClient extends Thread /*implements CommonClientActions*/ {
+public class SocketClient extends Thread implements CommonClientActions {
     //todo implement all common client actions methods
     private final String ip = "127.0.0.1";
     private final int port = 8888;
@@ -58,5 +61,69 @@ public class SocketClient extends Thread /*implements CommonClientActions*/ {
         out.writeObject(new AddPlayerMessage(nickname, numPlayers));
         out.flush();
         out.reset();
+    }
+
+    @Override
+    public void joinFirstGameAvailable(String nickname) throws IOException, NotBoundException {
+        out.writeObject(new AddPlayerMessage(nickname, 1));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void chooseColor(String nickname, String color) throws IOException {
+        out.writeObject(new ChooseColorMessage(nickname, color));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void chooseHiddenGoal(String nickname, int choice) throws IOException {
+        out.writeObject(new ChooseHiddenGoalMessage(nickname, choice));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void chooseInitialCardSide(String nickname, int choice) throws IOException {
+        out.writeObject(new ChooseInitialCardSideMessage(nickname, choice));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void drawCard(String nickname, int choice) throws IOException {
+        out.writeObject(new DrawCardMessage(nickname, choice));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void playCard(String nickname, int cardIndex, boolean front, int x, int y) throws IOException {
+        out.writeObject(new PlayCardMessage(nickname, cardIndex, front, x, y));
+        out.flush();
+        out.reset();
+    }
+
+    @Override
+    public void sendPublicMessage(String sender, String message) throws RemoteException {
+        try {
+            out.writeObject(new SendPublicMessageMessage(sender, message));
+            out.flush();
+            out.reset();
+        }
+        catch (IOException ignored){}
+
+    }
+
+    @Override
+    public void sendPrivateMessage(String sender, String receiver, String message) throws RemoteException {
+        try{
+            out.writeObject(new SendPrivateMessageMessage(sender, receiver, message));
+            out.flush();
+            out.reset();
+        }
+        catch (IOException ignored){}
+
     }
 }
