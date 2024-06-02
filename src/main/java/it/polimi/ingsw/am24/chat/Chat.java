295d6744b27330b36844495b0733a45fda0e7cc1
MattiaBelfiore <160751662+MattiaBelfiore@users.noreply.github.com>
2024-06-02 18:17:54 +0200

    chat package documentation

--- src/main/java/it/polimi/ingsw/am24/chat/Chat.java
+++ src/main/java/it/polimi/ingsw/am24/chat/Chat.java
@@ -3,30 +3,55 @@ package it.polimi.ingsw.am24.chat;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * The Chat class represents the functions connected to the chat present in the game
+ **/
 public class Chat implements Serializable {
     private List<ChatMessage> messages;
 
+    /**
+     * Constructor for the Chat class.
+     * Initializes the list of chat messages.
+     */
     public Chat() {
         this.messages = new ArrayList<ChatMessage>();
     }
 
+    /**
+     * Returns a list of chat messages as strings.
+     * @return a list of chat messages.
+     */
     public List<String> getMessages() {
         return messages.stream().map(m -> m.getMessage()).toList();
     }
 
+    /**
+     * Adds a public message to the chat.
+     * @param sender the sender of the message.
+     * @param message the content of the message.
+     */
     public void addPublicMessage(String sender, String message) {
         /*if (messages.size() > maxNumMessages)
             messages.removeFirst();*/
         messages.add(new ChatMessage(sender,message));
     }
 
+    /**
+     * Adds a private message to a specific user to the chat.
+     * @param sender the sender of the message.
+     * @param receiver the receiver of the message.
+     * @param message the content of the message.
+     */
     public void addPrivateMessage(String sender, String receiver, String message) {
         /*if (messages.size() > maxNumMessages)
             messages.removeFirst();*/
         messages.add(new ChatMessage(sender,receiver,message));
     }
 
+    /**
+     * Returns the last message in the chat.
+     * @return the last ChatMessage object.
+     */
     public ChatMessage getLast() {
         return messages.get(messages.size() - 1);
     }
--- src/main/java/it/polimi/ingsw/am24/chat/ChatMessage.java
+++ src/main/java/it/polimi/ingsw/am24/chat/ChatMessage.java
@@ -4,12 +4,21 @@ import java.io.Serializable;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The ChatMessage class represents a message in the chat.
+ * It contains information about the sender, receiver, message content, and the time the message was sent.
+ */
 public class ChatMessage implements Serializable {
     private String sender;
     private String receiver;
     private String message;
     private LocalTime time;
 
+    /**
+     * Constructor for creating a public chat message.
+     * @param sender the sender of the message.
+     * @param message the content of the message.
+     */
     public ChatMessage(String sender, String message) {
         this.sender = sender;
         this.receiver = "";
@@ -17,6 +26,12 @@ public class ChatMessage implements Serializable {
         this.time = java.time.LocalTime.now();;
     }
 
+    /**
+     * Constructor for creating a private chat message.
+     * @param sender the sender of the message.
+     * @param receiver the receiver of the message.
+     * @param message the content of the message.
+     */
     public ChatMessage(String sender, String receiver, String message) {
         this.sender = sender;
         this.receiver = receiver;
@@ -24,18 +39,34 @@ public class ChatMessage implements Serializable {
         this.time = java.time.LocalTime.now();;
     }
 
+    /**
+     * Returns the content of the message.
+     * @return the message content.
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Returns the sender of the message.
+     * @return the message sender.
+     */
     public String getSender() {
         return sender;
     }
 
+    /**
+     * Returns the receiver of the message.
+     * @return the message receiver.
+     */
     public String getReceiver() {
         return receiver;
     }
 
+    /**
+     * Returns the timestamp of the message.
+     * @return the message timestamp.
+     */
     public String getTime() {
         return time.format(DateTimeFormatter.ofPattern("HH:mm"));
     }
